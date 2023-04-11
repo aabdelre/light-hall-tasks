0fb46653ed7bbc45d33673c9ef3a1944c1e67b53
aabdelre <aabdelre@hamilton.edu>
2023-04-11 16:55:39 +0100

    reload bug fixed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 
 function App() {
   //localStorage.clear();
-  var [number, setCount] = useState(parseInt(localStorage.getItem("count")) + 1);
-  if(isNaN(localStorage.getItem("count"))) {
-    number = 0;
+  var [number, setCount] = useState(0);
+  if(localStorage.getItem("count")) {
+    number = parseInt(localStorage.getItem("count"));
   }
 
   function increment () {
-      setCount(number)
-      localStorage.setItem("count", number);
-      setCount(parseInt(number) + 1);
+      //setCount(number)
+      //localStorage.setItem("count", number);
+      number++;
+      setCount(parseInt(number));
       localStorage.setItem("count", number);
       console.log(number);
   }; 
